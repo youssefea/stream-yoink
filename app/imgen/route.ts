7f65d69d3c3677f0b9d89bdd534703473f423fa4
youssefea <youcefea99@gmail.com>
2024-03-20 18:42:09 +0000

    change images

--- app/imgen/route.ts
+++ app/imgen/route.ts
@@ -56,28 +56,34 @@ function generateSVG(text: string, color: string[], backgroundColor: string, siz
     `;
   }
 
-export async function GET(request: NextRequest) {
-
-  const { searchParams } = request.nextUrl;
-  const text = searchParams.get('text') || 'Default Text';
-  const color = searchParams.get('color')?.split(",") || new Array(10).fill('black');
-  const size = request.nextUrl.searchParams.get('size')?.split(",") || new Array(10).fill('12');
-  const backgroundColor = searchParams.get('background') || 'white';
-
-  // Validate inputs as needed
-  if (!text) {
-    return new NextResponse('Query parameter "text" is required', { status: 400 });
+  export async function GET(request: NextRequest) {
+    const { searchParams } = request.nextUrl;
+    const text = searchParams.get('text') || 'Default Text';
+    const color = searchParams.get('color')?.split(",") || new Array(10).fill('black');
+    const size = request.nextUrl.searchParams.get('size')?.split(",") || new Array(10).fill('12');
+    const backgroundColor = searchParams.get('background') || 'white';
+  
+    // Generate the SVG content
+    const svg = generateSVG(text, color, backgroundColor, size);
+  
+    // Convert SVG buffer to PNG with sharp, specifying higher DPI for better quality
+    const pngBuffer = await sharp(Buffer.from(svg), { density: 300 }) // Increase DPI for better quality
+      .resize({ width: 600 }) // Adjust width as needed, height is auto-scaled to maintain aspect ratio
+      .png({
+        quality: 100, // Set high quality for PNG output
+        compressionLevel: 9 // Use higher compression level for smaller file size with minimal quality loss
+      })
+      .toBuffer();
+  
+    // Return the PNG image in the response
+    return new NextResponse(pngBuffer, {
+      status: 200,
+      headers: {
+        'Content-Type': 'image/png',
+        'Cache-Control': 's-maxage=1, stale-while-revalidate',
+      },
+    });
   }
-
-  const svg = generateSVG(text, color, backgroundColor, size);
-  return new NextResponse(svg, {
-    status: 200,
-    headers: {
-      'Content-Type': 'image/svg+xml',
-      'Cache-Control': 's-maxage=1, stale-while-revalidate',
-    },
-  });
-}
 
 // Force the function to be considered dynamic to bypass any caching
 export const dynamic = 'force-dynamic';
