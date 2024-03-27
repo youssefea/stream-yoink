7b59d188d6c5fbd544f98d822cfa42f40148b910
youssefea <youcefea99@gmail.com>
2024-03-27 19:45:33 +0000

    adding fail safe to the current yoinker

--- app/currentYoinkerApi/route.ts
+++ app/currentYoinkerApi/route.ts
@@ -1,42 +1,42 @@
 // app/currentYoinkerApi/route.ts
-'use server'
-import { unstable_noStore as noStore } from 'next/cache';
+"use server";
+import { unstable_noStore as noStore } from "next/cache";
 
-import { NextResponse } from 'next/server';
-import { kv } from '@vercel/kv';
+import { NextResponse } from "next/server";
+import { kv } from "@vercel/kv";
+import { profile } from "console";
 
 export async function GET() {
-  noStore()
+  noStore();
   try {
     // Fetch the profileHandle of the current yoinker
-    const profileHandle = await kv.hget('currentYoinker', 'profileHandle');
-    const address= await kv.hget('currentYoinker', 'address');
+    let profileHandle = await kv.hget("currentYoinker", "profileHandle");
+    let address = await kv.hget("currentYoinker", "address");
 
     // If profileHandle is not found, return a meaningful error response
     if (!profileHandle) {
-      return new NextResponse(JSON.stringify({ error: 'Current yoinker not found' }), {
-        status: 404, // Not Found status
-        headers: {
-          'Content-Type': 'application/json', // Specify the content type
-        },
-      });
+      profileHandle = "fakeHandle";
+      address = "0xfabababababababababa740f03963281634847ba";
     }
 
     // Use NextResponse to return the profileHandle
     return new NextResponse(JSON.stringify({ profileHandle, address }), {
       status: 200, // OK status
       headers: {
-        'Content-Type': 'application/json', // Specify the content type
+        "Content-Type": "application/json", // Specify the content type
       },
     });
   } catch (error) {
     // Handle any errors that occur during the fetch operation
     console.error(error);
-    return new NextResponse(JSON.stringify({ error: 'Internal Server Error' }), {
-      status: 500, // Internal Server Error status
-      headers: {
-        'Content-Type': 'application/json', // Specify the content type
-      },
-    });
+    return new NextResponse(
+      JSON.stringify({ error: "Internal Server Error" }),
+      {
+        status: 500, // Internal Server Error status
+        headers: {
+          "Content-Type": "application/json", // Specify the content type
+        },
+      }
+    );
   }
 }
